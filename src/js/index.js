2ffe5112f59d50cb92d2671f26ac9e437ed502b8
George Mihov <gmihov001@gmail.com>
2019-08-31 00:27:31 +0000

    inital commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,18 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let top = ["&#9824", "&#9827", "&#9829", "&#9830"];
+  let middle = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let randomNumber = middle[Math.floor(Math.random() * middle.length)];
+  let randomSuite = top[Math.floor(Math.random() * top.length)];
+  console.log(randomSuite);
+
+  if (randomSuite === "&#9830" || randomSuite === "&#9829") {
+    document.querySelector("#top").style.color = "red";
+    document.querySelector("#middle").style.color = "red";
+    document.querySelector("#bottom").style.color = "red";
+  }
+  document.querySelector("#top").innerHTML = randomSuite;
+  document.querySelector("#middle").innerHTML = randomNumber;
+  document.querySelector("#bottom").innerHTML = randomSuite;
 };
