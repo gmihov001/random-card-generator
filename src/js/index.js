b4776ef12351088c9e5a01fe6fba945a0f86cdb6
George Mihov <gmihov001@gmail.com>
2019-09-12 20:57:21 +0000

    Final submission

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,15 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function(){
+window.onload = function() {
+  myFunction();
+};
+
+function myFunction() {
   let top = ["&#9824", "&#9827", "&#9829", "&#9830"];
   let middle = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
   let randomNumber = middle[Math.floor(Math.random() * middle.length)];
   let randomSuite = top[Math.floor(Math.random() * top.length)];
-  console.log(randomSuite);
 
   if (randomSuite === "&#9830" || randomSuite === "&#9829") {
     document.querySelector("#top").style.color = "red";
@@ -21,4 +24,9 @@ window.onload = function(){
   document.querySelector("#top").innerHTML = randomSuite;
   document.querySelector("#middle").innerHTML = randomNumber;
   document.querySelector("#bottom").innerHTML = randomSuite;
+}
+
+let sortBtn = document.getElementById("sort");
+sortBtn.onclick = function() {
+  myFunction();
 };
